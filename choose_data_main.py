1a505df0fa5f0dbb2111f840720eadcbc0ff0299
ypanpande <ypanbx02@gmail.com>
2022-05-31 12:23:46 +0200

    loc_sequential6s

--- choose_data_main.py
+++ choose_data_main.py
@@ -215,17 +215,18 @@ class FirstWin:
 # menu functions
 # =============================================================================
 
-    def loc_sequential6s(self):
-        ls6 = tk.Toplevel(self.root)
-        date = '{}-{:02}-{:02}'.format(self.beginDay['year_selected'],
-                                       self.beginDay['month_selected'], self.beginDay['day_selected'])
-        LocField(ls6, self.default_source_file, 'loc_seq6s', date)
 
     def loc_geiger6s(self):
         lg6 = tk.Toplevel(self.root)
         date = '{}-{:02}-{:02}'.format(self.beginDay['year_selected'],
                                        self.beginDay['month_selected'], self.beginDay['day_selected'])
         LocField(lg6, self.default_source_file, 'loc_geiger6s', date)
+
+    def loc_sequential6s(self):
+        ls6 = tk.Toplevel(self.root)
+        date = '{}-{:02}-{:02}'.format(self.beginDay['year_selected'],
+                                       self.beginDay['month_selected'], self.beginDay['day_selected'])
+        LocField(ls6, self.default_source_file, 'loc_seq6s', date)
 
     def loc_sequential4s(self):
         ls4 = tk.Toplevel(self.root)
